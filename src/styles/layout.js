d6813d7918d097caa4a0ca11208606099e04ae4e
CharlesD61994 <charles.dugre@live.ca>
2026-06-27 16:01:10 -0400

    Moderate iPhone top spacing

--- src/styles/layout.js
+++ src/styles/layout.js
@@ -1,3 +1,3 @@
-export const IOS_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 68px)";
-export const IOS_READER_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 44px)";
+export const IOS_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 46px)";
+export const IOS_READER_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 28px)";
 export const FLOATING_ACTION_BOTTOM = "calc(env(safe-area-inset-bottom, 0px) + 86px)";
