afe8fd35a252a2e13f7842e6cfee5201b72cce2e
CharlesD61994 <charles.dugre@live.ca>
2026-06-27 15:58:49 -0400

    Respect iPhone safe area spacing

--- src/styles/layout.js
+++ src/styles/layout.js
@@ -1,3 +1,3 @@
-export const IOS_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 58px)";
-export const IOS_READER_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 34px)";
+export const IOS_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 68px)";
+export const IOS_READER_TOP_PADDING = "calc(env(safe-area-inset-top, 0px) + 44px)";
 export const FLOATING_ACTION_BOTTOM = "calc(env(safe-area-inset-bottom, 0px) + 86px)";
