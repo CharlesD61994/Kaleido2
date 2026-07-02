ae4e3852d314d4f09fdd0d1449240bc9ba876fcb
CharlesD61994 <charles.dugre@live.ca>
2026-07-02 13:41:34 -0400

    Nudge native PDF timer up

--- ios/App/App/KaleidoPdfViewerPlugin.swift
+++ ios/App/App/KaleidoPdfViewerPlugin.swift
@@ -755,7 +755,7 @@ final class KaleidoNativePdfHeaderView: UIView {
         globalLabel.frame = CGRect(x: circleX, y: top + 27, width: circleSize, height: 18)
         circleView.frame = CGRect(x: circleX, y: globalLabel.frame.maxY + 2, width: circleSize, height: circleSize)
 
-        timerButton.frame = CGRect(x: bounds.width - right - 92, y: top + 26, width: 92, height: 20)
+        timerButton.frame = CGRect(x: bounds.width - right - 92, y: top + 23, width: 92, height: 20)
         timerMenu.frame = CGRect(x: bounds.width - right - 172, y: timerButton.frame.maxY + 8, width: 172, height: 86)
         timerMenuLabel.frame = CGRect(x: 10, y: 9, width: 152, height: 24)
         timerToggleButton.frame = CGRect(x: 10, y: 43, width: 72, height: 32)
