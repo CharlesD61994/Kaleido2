cdce3e719b4f703e677999e10b8cb5258b770430
CharlesD61994 <charles.dugre@live.ca>
2026-07-02 13:48:53 -0400

    Raise native PDF header toward safe area

--- ios/App/App/KaleidoPdfViewerPlugin.swift
+++ ios/App/App/KaleidoPdfViewerPlugin.swift
@@ -747,7 +747,7 @@ final class KaleidoNativePdfHeaderView: UIView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        let top = safeAreaInsets.top
+        let top = max(0, safeAreaInsets.top - 8)
         let left: CGFloat = 6
         let right: CGFloat = 20
         let circleSize: CGFloat = 95
